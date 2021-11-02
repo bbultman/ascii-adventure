cb44eb02c04f051af0bbf77227c9290cb6fa83c8
Ben Bultman <bultman.ben@gmail.com>
2021-11-02 12:37:16 +0100

    Make tiles possibly solid

--- src/Tile.ts
+++ src/Tile.ts
@@ -1,28 +1,31 @@
-import Color from "./Color";
-import Vector from "./Vector";
+import Color from './Color'
+import Vector from './Vector'
 
-interface TileConstructorOptions {
-  char?: string;
-  color?: Color;
-  background?: Color;
-  pos?: Vector;
-  isVisible?: boolean;
-};
+export interface TileConstructorOptions {
+  char: string
+  color: Color
+  background: Color
+  pos: Vector
+  isVisible: boolean
+  isSolid: boolean
+}
 
 export default class Tile {
-  char: string;
-  color: Color;
-  background: Color;
-  pos: Vector;
-  isVisible: boolean;
+  char: string
+  color: Color
+  background: Color
+  pos: Vector
+  isVisible: boolean
+  isSolid: boolean
 
-  readonly id: string = Math.random().toString(36).slice(2);
+  readonly id: string = Math.random().toString(36).slice(2)
 
-  constructor(options: TileConstructorOptions) {
-    this.char = options.char || ' ';
-    this.color = options.color || new Color();
-    this.background = options.background || new Color(0, 0, 0, 1);
-    this.pos = options.pos || new Vector(0, 0);
-    this.isVisible = options.isVisible || true;
+  constructor(options: Partial<TileConstructorOptions>) {
+    this.char = options.char ?? ' '
+    this.color = options.color ?? new Color()
+    this.background = options.background ?? new Color(0, 0, 0, 1)
+    this.pos = options.pos ?? new Vector(0, 0)
+    this.isVisible = options.isVisible ?? true
+    this.isSolid = options.isSolid ?? false
   }
 }
