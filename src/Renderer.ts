ae40d3a45e39cce69256e90147ef78b85d01c129
Ben Bultman <bultman.ben@gmail.com>
2021-11-02 09:31:53 +0100

    Initial commit

--- src/Renderer.ts
+++ src/Renderer.ts
@@ -1,84 +1,86 @@
-import Layer from "./Layer";
+import Layer from './Layer'
 
 export default class Renderer {
-  private namedLayers: Record<string, Layer> = {};
-  private layers: Array<Layer> = [];
-  private layerElements: Record<string, HTMLElement> = {};
-  private size: number = 30;
-  private beforeDraw: () => void = () => {};
+  private namedLayers: Record<string, Layer> = {}
+  private layers: Array<Layer> = []
+  private layerElements: Record<string, HTMLElement> = {}
+  private size: number = 30
+  private beforeDraw: () => void = () => {}
 
-  frames:number = 0;
+  frames: number = 0
 
   setSize(n: number) {
-    this.size = n;
+    this.size = n
   }
 
   addLayer(name: string, layer: Layer) {
     if (name in this.namedLayers) {
-      return new Error(`${name} layer already attached to renderer`);
+      return new Error(`${name} layer already attached to renderer`)
     }
-    this.namedLayers[name] = layer;
-    this.layers.push(layer);
+    this.namedLayers[name] = layer
+    this.layers.push(layer)
 
-    this.orderLayers();
+    this.orderLayers()
 
-    return this;
+    return this
   }
 
   onBeforeDraw(cb: () => void) {
-    this.beforeDraw = cb;
+    this.beforeDraw = cb
   }
 
   commit() {
-    this.beforeDraw();
+    this.beforeDraw()
 
     for (let [name, layer] of Object.entries(this.namedLayers)) {
-      let layerEl = this.layerElements[name];
+      let layerEl = this.layerElements[name]
 
       if (!layerEl) {
-        layerEl = document.createElement('div');
-        layerEl.classList.add('asc-engine-layer');
-        layerEl.style.fontSize = `${this.size}px`;
-        layerEl.style.top = `${layer.pos.y * this.size}px`;
-        layerEl.style.left = `${layer.pos.x * this.size / 2}px`;
-        layerEl.style.height = `${layer.size.y * this.size}px`;
-        layerEl.style.width = `${layer.size.x * this.size / 2}px`;
-        layerEl.style.zIndex = layer.z.toString();
+        layerEl = document.createElement('div')
+        layerEl.classList.add('asc-engine-layer')
+        layerEl.style.fontSize = `${this.size}px`
+        layerEl.style.top = `${layer.pos.y * this.size}px`
+        layerEl.style.left = `${(layer.pos.x * this.size) / 2}px`
+        layerEl.style.height = `${layer.size.y * this.size}px`
+        layerEl.style.width = `${(layer.size.x * this.size) / 2}px`
+        layerEl.style.zIndex = layer.z.toString()
 
-        document.getElementById('asc-engine-layer-container').appendChild(layerEl);
-        this.layerElements[name] = layerEl;
+        document.getElementById('asc-engine-layer-container').appendChild(layerEl)
+        this.layerElements[name] = layerEl
       }
 
       for (let op of layer.operations) {
-        let opEl = document.getElementById(`asc-engine-tile-${op.tile.id}`);
+        let opEl = document.getElementById(`asc-engine-tile-${op.tile.id}`)
 
         if (!opEl) {
-          opEl = document.createElement('div');
-          opEl.classList.add('asc-engine-tile');
-          opEl.id = `asc-engine-tile-${op.tile.id}`;
+          opEl = document.createElement('div')
+          opEl.classList.add('asc-engine-tile')
+          opEl.id = `asc-engine-tile-${op.tile.id}`
 
-          layerEl.appendChild(opEl);
+          layerEl.appendChild(opEl)
         }
 
+        opEl.innerHTML = op.char.replace(/ /g, '&nbsp;')
+        opEl.style.top = `${op.pos.y * this.size}px`
+        opEl.style.left = `${(op.pos.x * this.size) / 2}px`
+        opEl.style.display = 'block'
+
         if (op.isVisible) {
-          opEl.innerHTML = op.char.replace(/ /g, '&nbsp;');
-          opEl.style.color = op.color.toCssString();
-          opEl.style.backgroundColor = op.background.toCssString();
-          opEl.style.top = `${op.pos.y * this.size}px`;
-          opEl.style.left = `${op.pos.x * this.size / 2}px`;
-          opEl.style.display = 'block';
+          opEl.style.color = op.color.toCssString()
+          opEl.style.backgroundColor = op.background.toCssString()
         } else {
-          opEl.style.display = 'none';
+          opEl.style.color = 'black'
+          opEl.style.backgroundColor = 'black'
         }
       }
 
-      layer.clear();
+      layer.clear()
     }
 
-    this.frames++;
+    this.frames++
   }
 
   private orderLayers() {
-    this.layers = this.layers.sort((la, lb) => la.z - lb.z);
+    this.layers = this.layers.sort((la, lb) => la.z - lb.z)
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,76 +1,105 @@
-import Color from "./Color";
-import Layer from "./Layer";
-import Renderer from "./Renderer";
-import Tile from "./Tile";
-import Vector from "./Vector";
+import Color from './Color'
+import Layer from './Layer'
+import Renderer from './Renderer'
+import Tile from './Tile'
+import Vector from './Vector'
 
-const WIDTH = 80;
-const HEIGHT = 24;
+const WIDTH = 80
+const HEIGHT = 24
 
 const layers: Record<string, Layer> = {
   background: new Layer({ size: new Vector(WIDTH, HEIGHT) }),
   actor: new Layer({ size: new Vector(WIDTH, HEIGHT) }),
-};
+}
 
 const player = new Tile({
-  background: new Color(0,0,0,0),
+  background: new Color(0, 0, 0, 0),
   char: '@',
   color: new Color(255, 0, 0),
   isVisible: true,
-  pos: Vector.Zero()
-});
+  pos: new Vector(WIDTH / 2, HEIGHT / 2),
+})
 
-const backgroundTiles = Array.from({ length: WIDTH*HEIGHT }, (_, i) => {
-  const x = i % WIDTH;1
-  const y = Math.floor(i / WIDTH);
+const backgroundTiles = Array.from({ length: WIDTH * HEIGHT }, (_, i) => {
+  const x = i % WIDTH
+  1
+  const y = Math.floor(i / WIDTH)
 
   return new Tile({
-    char: '.',
-    pos: new Vector(x, y)
-  });
-});
-
-const renderer = new Renderer();
-renderer.setSize(35);
-renderer.addLayer('background', layers.background);
-renderer.addLayer('actor', layers.actor);
-
-
-renderer.onBeforeDraw(() => {
-  layers.background.operations.forEach(op => {
-    const newAlpha = (Math.sin(op.pos.x / op.pos.y + 5) + 1) / 2;
-    op.color.a = newAlpha;
+    char: x === 0 || x === WIDTH - 1 ? '#' : y === 0 || y === HEIGHT - 1 ? '#' : '.',
+    pos: new Vector(x, y),
   })
 })
 
+const getDistance = (v1: Vector, v2: Vector) =>
+  Math.round(Math.sqrt(Math.pow(v1.x - v2.x, 2) + Math.pow(v1.y - v2.y, 2)))
+
+const renderer = new Renderer()
+renderer.setSize(20)
+renderer.addLayer('background', layers.background)
+renderer.addLayer('actor', layers.actor)
+
+renderer.onBeforeDraw(() => {
+  layers.background.operations.forEach((tile) => {
+    if (tile.pos.y === player.pos.y && tile.pos.x === player.pos.x) {
+      // Dont render ground under char's feet
+      tile.color.a = 0
+    }
+
+    const playerTileDistance = getDistance(player.pos, tile.pos)
+
+    if (playerTileDistance > 10) {
+      tile.isVisible = false
+      return
+    }
+
+    if (playerTileDistance > 5) {
+      tile.isVisible = true
+      tile.color.a = 0.4
+      return
+    }
+
+    tile.isVisible = true
+  })
+})
 
 const draw = () => {
-  backgroundTiles.forEach(tile => layers.background.draw(tile));
-  layers.actor.draw(player);
-  renderer.commit();
-
-  requestAnimationFrame(draw);
+  backgroundTiles.forEach((tile) => layers.background.draw(tile))
+  layers.actor.draw(player)
+  renderer.commit()
 }
 
-draw();
+let ready = false
 
-document.addEventListener('keydown', e => {
+document.addEventListener('readystatechange', () => {
+  if (!ready) {
+    draw()
+    ready = true
+  }
+})
+
+document.addEventListener('keydown', (e) => {
   switch (e.key) {
     case 'ArrowUp': {
-      player.pos.add(new Vector(0, -1));
-      break;
+      player.pos.add(new Vector(0, -1))
+      draw()
+      break
     }
     case 'ArrowDown': {
-      player.pos.add(new Vector(0, 1));
-      break;
+      player.pos.add(new Vector(0, 1))
+      draw()
+      break
     }
     case 'ArrowLeft': {
-      player.pos.add(new Vector(-1, 0));
-      break;
+      player.pos.add(new Vector(-1, 0))
+      draw()
+      break
     }
     case 'ArrowRight': {
-      player.pos.add(new Vector(1, 0));
-      break;
+      player.pos.add(new Vector(1, 0))
+      draw()
+      break
     }
   }
 })
+
