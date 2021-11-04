03bddf4fd02bbdf8a753a48b3ab0c32acef46341
Ben Bultman <bultman.ben@gmail.com>
2021-11-04 09:20:34 +0100

    Render drop operations correctly, fallback to invisible tiles

--- src/Renderer.ts
+++ src/Renderer.ts
@@ -1,86 +1,122 @@
-import Layer from './Layer'
+import { GameViews } from "./index";
+import Layer from "./Layer";
+import Tile from "./Tile";
+
+type RendererParams = GameViews;
 
 export default class Renderer {
-  private namedLayers: Record<string, Layer> = {}
-  private layers: Array<Layer> = []
-  private layerElements: Record<string, HTMLElement> = {}
-  private size: number = 30
-  private beforeDraw: () => void = () => {}
+  namedLayers: Record<string, Layer> = {};
+  private layers: Array<Layer> = [];
+  private layerElements: Record<string, HTMLElement> = {};
+  private size: number = 30;
+  private beforeDraw: (layer: Layer) => void;
 
-  frames: number = 0
+  operations: number = 0
+  frames: number = 0;
+
+  constructor(views: RendererParams) {
+    views.main.layers.forEach((layer) => this.addLayer(layer));
+    views.info.layers.forEach((layer) => this.addLayer(layer));
+  }
 
   setSize(n: number) {
-    this.size = n
+    this.size = n;
   }
 
-  addLayer(name: string, layer: Layer) {
-    if (name in this.namedLayers) {
-      return new Error(`${name} layer already attached to renderer`)
+  addLayer(layer: Layer) {
+    if (layer.name in this.namedLayers) {
+      return new Error(`${name} layer already attached to renderer`);
     }
-    this.namedLayers[name] = layer
-    this.layers.push(layer)
+    this.namedLayers[layer.name] = layer;
+    this.layers.push(layer);
 
-    this.orderLayers()
+    this.orderLayers();
 
-    return this
+    return this;
   }
 
-  onBeforeDraw(cb: () => void) {
-    this.beforeDraw = cb
+  onBeforeDraw(cb: (layer: Layer) => void) {
+    this.beforeDraw = cb;
+  }
+
+  // TODO Only draw tiles that have changed since last frame
+  draw(layerName: string, tile: Tile) {
+    if (!tile) return
+    this.namedLayers[layerName].draw(tile);
+  }
+
+  drawAll(layerName: string, tiles: Tile[]) {
+    tiles.forEach((tile) => this.draw(layerName, tile));
+  }
+
+  drop(layerName: string, tile: Tile) {
+    this.namedLayers[layerName].drop(tile)
   }
 
   commit() {
-    this.beforeDraw()
+    this.layers.forEach((layer) => {
+      this.beforeDraw(layer);
+    });
 
     for (let [name, layer] of Object.entries(this.namedLayers)) {
-      let layerEl = this.layerElements[name]
+      let layerEl = this.layerElements[name];
 
       if (!layerEl) {
-        layerEl = document.createElement('div')
-        layerEl.classList.add('asc-engine-layer')
-        layerEl.style.fontSize = `${this.size}px`
-        layerEl.style.top = `${layer.pos.y * this.size}px`
-        layerEl.style.left = `${(layer.pos.x * this.size) / 2}px`
-        layerEl.style.height = `${layer.size.y * this.size}px`
-        layerEl.style.width = `${(layer.size.x * this.size) / 2}px`
-        layerEl.style.zIndex = layer.z.toString()
+        layerEl = document.createElement("div");
+        layerEl.classList.add("asc-engine-layer");
+        layerEl.style.fontSize = `${this.size / 1.1}px`;
+        layerEl.style.top = `${layer.pos.y * this.size}px`;
+        layerEl.style.left = `${(layer.pos.x * this.size) / 2}px`;
+        layerEl.style.height = `${layer.size.y * this.size}px`;
+        layerEl.style.width = `${(layer.size.x * this.size) / 2}px`;
+        layerEl.style.zIndex = layer.z.toString();
 
-        document.getElementById('asc-engine-layer-container').appendChild(layerEl)
-        this.layerElements[name] = layerEl
+        document
+          .getElementById("asc-engine-layer-container")
+          .appendChild(layerEl);
+        this.layerElements[name] = layerEl;
       }
 
       for (let op of layer.operations) {
-        let opEl = document.getElementById(`asc-engine-tile-${op.tile.id}`)
+        let opEl = document.getElementById(`asc-engine-tile-${op.tile.id}`);
+
+        if (op.delete === true) {
+          layerEl.removeChild(
+            document.getElementById(`asc-engine-tile-${op.tile.id}`)
+          );
+        }
 
         if (!opEl) {
-          opEl = document.createElement('div')
-          opEl.classList.add('asc-engine-tile')
-          opEl.id = `asc-engine-tile-${op.tile.id}`
+          opEl = document.createElement("div");
+          opEl.classList.add("asc-engine-tile");
+          opEl.id = `asc-engine-tile-${op.tile.id}`;
 
-          layerEl.appendChild(opEl)
+          layerEl.appendChild(opEl);
         }
 
-        opEl.innerHTML = op.char.replace(/ /g, '&nbsp;')
-        opEl.style.top = `${op.pos.y * this.size}px`
-        opEl.style.left = `${(op.pos.x * this.size) / 2}px`
-        opEl.style.display = 'block'
+        opEl.innerHTML = op.char.replace(/ /g, "&nbsp;");
+        opEl.style.top = `${op.pos.y * this.size}px`;
+        opEl.style.left = `${(op.pos.x * this.size) / 2}px`;
+        opEl.style.display = "block";
 
         if (op.isVisible) {
-          opEl.style.color = op.color.toCssString()
-          opEl.style.backgroundColor = op.background.toCssString()
+          opEl.style.color = op.color.toCssString();
+          opEl.style.backgroundColor = op.background.toCssString();
         } else {
-          opEl.style.color = 'black'
-          opEl.style.backgroundColor = 'black'
+          opEl.style.color = "black";
+          opEl.style.backgroundColor = "black";
         }
+
+        this.operations++
       }
 
-      layer.clear()
+      layer.clear();
     }
 
-    this.frames++
+    this.frames++;
   }
 
   private orderLayers() {
-    this.layers = this.layers.sort((la, lb) => la.z - lb.z)
+    this.layers = this.layers.sort((la, lb) => la.z - lb.z);
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,135 +1,104 @@
-import Color from './Color'
-import Layer from './Layer'
-import { makeGroundTile, makeSolidTile } from './Objects'
-import Renderer from './Renderer'
-import Tile from './Tile'
-import Vector from './Vector'
-import World from './World'
+import Layer from "./Layer";
+import { makeGroundTile, makeSolidTile } from "./Objects";
+import Renderer from "./Renderer";
+import Vector from "./Vector";
+import World from "./World";
+import buildInputHandler from "./Input";
 
-const WIDTH = 80
-const HEIGHT = 24
-
-const layers: Record<string, Layer> = {
-  background: new Layer({ size: new Vector(WIDTH, HEIGHT) }),
-  actor: new Layer({ size: new Vector(WIDTH, HEIGHT) }),
-}
-
-const player = new Tile({
-  background: new Color(0, 0, 0, 0),
-  char: '@',
-  color: new Color(255, 0, 0),
-  isVisible: true,
-  pos: new Vector(WIDTH / 2, HEIGHT / 2),
-})
-
-const world = new World()
-
-const backgroundTiles = world.tiles
+const WIDTH = 80;
+const HEIGHT = 24;
 
 const getDistance = (v1: Vector, v2: Vector) =>
-  Math.round(Math.sqrt(Math.pow(v1.x - v2.x, 2) + Math.pow(v1.y - v2.y, 2)))
+  Math.round(Math.sqrt(Math.pow(v1.x - v2.x, 2) + Math.pow(v1.y - v2.y, 2)));
 
-const renderer = new Renderer()
-renderer.setSize(20)
-renderer.addLayer('background', layers.background)
-renderer.addLayer('actor', layers.actor)
+type GameView = {
+  name: string;
+  size: Vector;
+  layers: Layer[];
+};
 
-renderer.onBeforeDraw(() => {
-  layers.background.operations.forEach((tile) => {
-    if (tile.pos.y === player.pos.y && tile.pos.x === player.pos.x) {
-      // Dont render ground under char's feet
-      tile.color.a = 0
+export type GameViews = {
+  main: GameView;
+  info: GameView;
+};
+
+const gameViews: GameViews = {
+  main: {
+    name: "background",
+    size: new Vector(WIDTH, HEIGHT),
+    layers: [
+      new Layer({ name: "background", size: new Vector(WIDTH, HEIGHT) }),
+      new Layer({ name: "actor", size: new Vector(WIDTH, HEIGHT) }),
+    ],
+  },
+  info: {
+    name: "info",
+    size: new Vector(WIDTH, 10),
+    layers: [
+      new Layer({
+        name: "info",
+        size: new Vector(WIDTH, 5),
+        pos: new Vector(0, HEIGHT),
+      }),
+    ],
+  },
+};
+
+const renderer = new Renderer(gameViews);
+
+renderer.onBeforeDraw(function (layer: Layer) {
+  console.log('before draw, operation count', this.operations)
+  if (layer.name === "info") return
+
+  layer.operations.forEach((tile) => {
+    if (layer.name === "background") {
+      if (
+        tile.pos.y === world.player.position.y &&
+        tile.pos.x === world.player.position.x
+      ) {
+        // Dont render ground under char's feet
+        tile.color.a = 0;
+      }
     }
 
-    const playerTileDistance = getDistance(player.pos, tile.pos)
+    // TODO Figure out how to make 'light' respect solid tiles (vector angle calculations?)
+    // TODO Make this generic so it can be applied on objects as well
+    const playerTileDistance = getDistance(world.player.position, tile.pos);
 
     if (playerTileDistance > 10) {
-      tile.isVisible = false
-      return
+      tile.isVisible = false;
+      return;
     }
 
     if (playerTileDistance > 5) {
-      tile.isVisible = true
-      tile.color.a = 0.4
-      return
+      tile.isVisible = true;
+      tile.color.a = 0.4;
+      return;
     }
 
-    tile.isVisible = true
-  })
-})
+    tile.isVisible = true;
+  });
+});
 
 const draw = () => {
-  backgroundTiles.forEach((tile) => layers.background.draw(tile))
-  layers.actor.draw(player)
-  renderer.commit()
-}
+  renderer.drawAll("background", world.background);
+  renderer.draw("actor", world.player.tile);
+  renderer.drawAll(
+    "actor",
+    world.mobs.map((mob) => mob && mob.tile)
+  );
+  renderer.drawAll("info", world.info);
+  renderer.commit();
+};
 
-let ready = false
+const world = new World(gameViews, draw, renderer.drop.bind(renderer));
 
-document.addEventListener('readystatechange', () => {
+let ready = false;
+
+document.addEventListener("keydown", buildInputHandler(world));
+document.addEventListener("readystatechange", () => {
   if (!ready) {
-    draw()
-    ready = true
+    ready = true;
+    draw();
   }
-})
-
-document.addEventListener('keydown', (e) => {
-  switch (e.key) {
-    case 'ArrowUp': {
-      const newPlayerPos = player.pos.clone().add(new Vector(0, -1))
-      // How do we get a bg tile by coordinates quicker than O(n)?
-      const bgRef = backgroundTiles.find((bg) => bg.pos.x === newPlayerPos.x && bg.pos.y === newPlayerPos.y)
-
-      if (bgRef.isSolid) {
-        console.log('Ouch!')
-        return
-      }
-
-      player.pos = newPlayerPos
-      draw()
-      break
-    }
-    case 'ArrowDown': {
-      const newPlayerPos = player.pos.clone().add(new Vector(0, 1))
-      // How do we get a bg tile by coordinates quicker than O(n)?
-      const bgRef = backgroundTiles.find((bg) => bg.pos.x === newPlayerPos.x && bg.pos.y === newPlayerPos.y)
-
-      if (bgRef.isSolid) {
-        console.log('Ouch!')
-        return
-      }
-
-      player.pos = newPlayerPos
-      draw()
-      break
-    }
-    case 'ArrowLeft': {
-      const newPlayerPos = player.pos.clone().add(new Vector(-1, 0))
-      // How do we get a bg tile by coordinates quicker than O(n)?
-      const bgRef = backgroundTiles.find((bg) => bg.pos.x === newPlayerPos.x && bg.pos.y === newPlayerPos.y)
-
-      if (bgRef.isSolid) {
-        console.log('Ouch!')
-        return
-      }
-
-      player.pos = newPlayerPos
-      draw()
-      break
-    }
-    case 'ArrowRight': {
-      const newPlayerPos = player.pos.clone().add(new Vector(1, 0))
-      // How do we get a bg tile by coordinates quicker than O(n)?
-      const bgRef = backgroundTiles.find((bg) => bg.pos.x === newPlayerPos.x && bg.pos.y === newPlayerPos.y)
-
-      if (bgRef.isSolid) {
-        console.log('Ouch!')
-        return
-      }
-
-      player.pos = newPlayerPos
-      draw()
-      break
-    }
-  }
-})
+});
